b90a692845b23c6828855eba43d3838edb603b53
Reza Aditama <aditamareza03@gmail.com>
2025-07-06 16:29:36 +0700

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className='bg-blue-900 flex min-h-screen justify-center items-center'>
+      <p>Hello World</p>
+    </div>
+  );
+};
+
+export default App;
